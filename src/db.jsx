03ce643443f4b49c1341d37c3b580ae62378d2cf
HobotM <mateuszhobot@gmail.com>
2023-04-08 00:53:52 +0100

    fix

--- src/db.jsx
+++ src/db.jsx
@@ -1,43 +1,59 @@
+import { useEffect, useState } from 'react';
 import Dexie from 'dexie';
-import {useLiveQuery} from 'dexie-react-hooks';
 
 export const db = new Dexie('todo-photos');
 db.version(1).stores({
-    photos: 'id'
+  photos: 'id',
 });
 
-async function addPhoto(id, imgSrc){
-    try{
-        const i = await db.photos.add({
-            id: id,
-            imgSrc: imgSrc
-        });
-    }catch (error){
-        console.log("failed to add pic")
-    }
-    return <>
-    <p>
+async function addPhoto(id, imgSrc) {
+  try {
+    const i = await db.photos.add({
+      id: id,
+      imgSrc: imgSrc,
+    });
+  } catch (error) {
+    console.log('failed to add pic');
+  }
+  return (
+    <>
+      <p>
         {imgSrc.length} &nbsp; | &nbsp; {id}
-    </p>
+      </p>
     </>
-};
+  );
+}
 
 async function deletePhoto(id) {
   try {
     await db.photos.delete(id);
   } catch (error) {
-    console.log("Failed to delete photo");
+    console.log('Failed to delete photo');
   }
 }
 
 function usePhotoSrc(id) {
-  const img = useLiveQuery(() => db.photos.where("id").equals(id).toArray());
+  const [imgSrc, setImgSrc] = useState(null);
 
-  if (Array.isArray(img) && img.length > 0) {
-    return img[0].imgSrc;
-  } else {
-    return null;
-  }
+  useEffect(() => {
+    db.photos
+      .where('id')
+      .equals(id)
+      .toArray()
+      .then((imgs) => {
+        if (imgs.length > 0) {
+          setImgSrc(imgs[0].imgSrc);
+        } else {
+          setImgSrc(null);
+        }
+      })
+      .catch((error) => {
+        console.log('Failed to get photo source', error);
+        setImgSrc(null);
+      });
+  }, [id]);
+
+  return imgSrc;
 }
 
-export { addPhoto, usePhotoSrc, deletePhoto };
+export { addPhoto, deletePhoto, usePhotoSrc };
