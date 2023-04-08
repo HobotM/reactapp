bdf29d8f306faf19d3514a5e538e8eb007b6d937
HobotM <mateuszhobot@gmail.com>
2023-04-08 10:36:22 +0100

    y

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -14,7 +14,6 @@ const WebcamCapture = (props) => {
     setImgSrc(imageSrc);
   };
 
-
   const savePhoto = async () => {
     if (imgSrc) {
       // Remove the previous image, if there is any
@@ -89,7 +88,6 @@ export default function Todo(props) {
     };
     fetchImage();
   }, [props.id]);
-  
 
   function handleChange(e) {
     setNewName(e.target.value);
@@ -167,10 +165,12 @@ export default function Todo(props) {
         <label className="todo-label" htmlFor={`${props.id}-closed`}>
           Closed
         </label>
-        <label className="todo-label" htmlFor={props.id}>
-          {props.name} from {props.city} | la {props.latitude} | lo{" "}
-          {props.longitude}
-        </label>
+        <div className="c-cb">
+          <label className="todo-label">
+            {props.name} from {props.city} | la {props.latitude} | lo{" "}
+            {props.longitude}
+          </label>
+        </div>
       </div>
       <div className="btn-group">
         <button type="button" className="btn" onClick={() => setEditing(true)}>
@@ -221,9 +221,5 @@ export default function Todo(props) {
     </div>
   );
 
-  return (
-    <li className="todo">
-      {isEditing ? editingTemplate : viewTemplate}
-    </li>
-  );
+  return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 }
