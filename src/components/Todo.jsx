d5aa4877711b18c28fe73d3ead9a981b5e17b0d0
HobotM <mateuszhobot@gmail.com>
2023-04-08 10:31:27 +0100

    label

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -109,9 +109,9 @@ export default function Todo(props) {
   const editingTemplate = (
     <form className="stack-small" onSubmit={handleSubmit}>
       <div className="form-group">
-        <label className="todo-label" htmlFor={props.id}>
+        {/* <label className="todo-label" htmlFor={props.id}>
           New name for {props.name}
-        </label>
+        </label> */}
         <input
           id={props.id}
           className="todo-text"
