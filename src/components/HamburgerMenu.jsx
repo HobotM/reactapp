a8cf83d6ae7ae21f5706aa2e24664ceec42bc31e
HobotM <mateuszhobot@gmail.com>
2023-04-09 17:53:47 +0100

    reset

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,23 +1,16 @@
 import React, { useState } from "react";
 import { FiMenu } from "react-icons/fi";
 import "reactjs-popup/dist/index.css";
-import AboutModal from "./AboutModal";
+
 
 const HamburgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [isAboutModalOpen, setIsAboutModalOpen] = useState(false);
+ 
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
-  const openAboutModal = () => {
-    setIsAboutModalOpen(true);
-  };
-
-  const closeAboutModal = () => {
-    setIsAboutModalOpen(false);
-  };
 
   return (
     <div className={`hamburger-menu${isOpen ? " open" : ""}`}>
